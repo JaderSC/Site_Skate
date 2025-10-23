0d741fb7d91bcfc1ddbf9a2088b9ac7315ed2928
JaderSC <jader.sirqueira@hotmail.com>
2025-10-23 12:22:59 -0300

    Adicionando o JavaScript na Página

---
+++ script.js
@@ -0,0 +1,38 @@
+const form = document.getElementById("form");
+const nome = document.getElementById("nome");
+const tel = document.getElementById("tel");
+const email = document.getElementById("email");
+const duvida = document.getElementById("duvida");
+
+
+
+
+form.addEventListener("submit", (evento) => {
+    evento.preventDefault();
+    inputNome();
+    inputTel();
+    inputEmail();
+    inputDuvida();
+   
+});
+
+function inputNome(){
+    const recebeNome = nome.value;
+    console.log(recebeNome);
+}
+
+function inputTel(){
+    const recebeTel = tel.value;
+    console.log(recebeTel);
+}
+
+function inputEmail(){
+    const recebeEmail = email.value;
+    console.log(recebeEmail);
+}
+
+function inputDuvida(){
+    const recebeDuvida = duvida.value;
+    console.log(recebeDuvida);
+}
+
